9c26d21f0ef3655919c5d404b6b23ffd74e7fcc7
mnlmartinez <mnlmartinez68@gmail.com>
2017-06-04 17:06:39 +0200

    update

---
+++ src/es/cbikesim/app/view/game/game.java
@@ -0,0 +1,16 @@
+package es.cbikesim.app.view.game;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class game extends Application{
+
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
